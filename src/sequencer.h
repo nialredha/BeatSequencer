3acb63e85bab8c0ccfe3b32d113980af021e678c
nialredha <nialredha@gmail.com>
2022-03-29 11:53:26 -0500

    Define default sequencer values

--- src/sequencer.h
+++ src/sequencer.h
@@ -8,6 +8,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define DEFAULT_BPM 240			// beats per minute
+#define DEFAULT_TIME_SIG 4		// beats per bar
+#define DEFAULT_NUM_BARS 16		// total number of bars
+#define DEFAULT_NUM_BEATS 64	// total number of beats (num_bars*time_sig)
+#define DEFAULT_RESOLUTION 0	// number of beats between each beat
+
 void load_default_samples();	// Load Default Samples (kick, hihat, snare)
 void load_samples();			// Load Samples
 void set_bpm();					// Set BPM
